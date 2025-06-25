82aa63e1b948551ce00bbe65fd8582029d5cda9f
Antoine Coilliaux <antoine.coilliaux@gmail.com>
2025-06-25 13:10:11 +0200

    Initial Commit

---
+++ CountryInfoTests/CountryInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryInfoTests.swift
+//  CountryInfoTests
+//
+//  Created by Antoine Coilliaux on 25/06/2025.
+//
+
+import Testing
+@testable import CountryInfo
+
+struct CountryInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
